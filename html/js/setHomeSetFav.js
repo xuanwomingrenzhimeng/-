aafa18b7f7974c52f9df6dcb4ab5967b07154f2c
seth <1174769620@qq.com>
2016-05-13 21:47:55 +0800

    进行重新管理,html文件夹作为校赛参加的作品,之后html+数字的为平常的练习,之后html的界面编写都是在这里完成.

---
+++ html/js/setHomeSetFav.js
@@ -0,0 +1,53 @@
+//加入收藏
+
+function AddFavorite(sURL, sTitle)
+{
+    
+    sURL = encodeURI(sURL);
+    try
+    {
+        
+        window.external.addFavorite(sURL, sTitle);
+        
+    }
+    catch (e)
+    {
+        
+        try
+        {
+            
+            window.sidebar.addPanel(sTitle, sURL, "");
+            
+        }
+        catch (e)
+        {
+            
+            alert("¼ÓÈëÊÕ²ØÊ§°Ü£¬ÇëÊ¹ÓÃCtrl+D½øÐÐÌí¼Ó,»òÊÖ¶¯ÔÚä¯ÀÀÆ÷Àï½øÐÐÉèÖÃ.");
+            
+        }
+        
+    }
+    
+}
+
+//设置首页
+
+function SetHome(url)
+{
+    
+    if (document.all)
+    {
+        
+        document.body.style.behavior = 'url(#default#homepage)';
+        
+        document.body.setHomePage(url);
+        
+    }
+    else
+    {
+        
+        alert("ÄúºÃ,ÄúµÄä¯ÀÀÆ÷²»Ö§³Ö×Ô¶¯ÉèÖÃÒ³ÃæÎªÊ×Ò³¹¦ÄÜ,ÇëÄúÊÖ¶¯ÔÚä¯ÀÀÆ÷ÀïÉèÖÃ¸ÃÒ³ÃæÎªÊ×Ò³!");
+        
+    }
+    
+}
